b0add1f3fb4e7899d4044733d37940814619b19d
yuji2005yy <yuji2005yy@gmail.com>
2026-01-15 23:04:15 +0900

    [Add] スコアを加算する機能を追加

---
+++ Assets/script/1224/Score.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using UnityEngine;
+using TMPro;
+
+public class Score : MonoBehaviour
+{
+    Enemy _Enemy;
+    [SerializeField] TextMeshProUGUI text;
+    [SerializeField] TextMeshProUGUI _highS;
+
+    int prevScore;
+    Coroutine scaleCoroutine;
+
+    void Start()
+    {
+        _Enemy = FindObjectOfType<Enemy>();
+
+        prevScore = _Enemy._score;
+        text.text = prevScore.ToString();
+        _highS.text = _Enemy._highScore.ToString();
+    }
+
+    void Update()
+    {
+        // 変わった瞬間だけ
+        if (_Enemy._score != prevScore)
+        {
+            prevScore = _Enemy._score;
+            text.text = prevScore.ToString();
+
+            // 連続加点対策
+            if (scaleCoroutine != null)
+                StopCoroutine(scaleCoroutine);
+
+            scaleCoroutine = StartCoroutine(ScaleAnim());
+        }
+
+        _highS.text = _Enemy._highScore.ToString();
+    }
+    /// <summary>
+    /// 拡縮アニメ
+    /// </summary>
+    /// <returns></returns>
+    IEnumerator ScaleAnim()
+    {
+        Vector3 baseScale = Vector3.one;
+        Vector3 bigScale = baseScale * 1.3f;
+
+        text.transform.localScale = bigScale;
+
+        yield return new WaitForSeconds(0.15f);
+
+        text.transform.localScale = baseScale;
+    }
+}
