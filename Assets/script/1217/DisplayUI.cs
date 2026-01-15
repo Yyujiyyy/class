00be9f71b916f8ace1d9b8b22e2041da3b23bbb7
yuji2005yy <yuji2005yy@gmail.com>
2026-01-15 09:45:13 +0900

    [Add] Imageを表示するためのスクリプトを追加

---
+++ Assets/script/1217/DisplayUI.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class DisplayUI : MonoBehaviour
+{
+    Renderer r;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        r = GetComponent<Renderer>();
+        r.enabled = false;
+    }
+}
