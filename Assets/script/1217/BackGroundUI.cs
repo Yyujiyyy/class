4ba38be1e9c2bb24715c6f4bdbffebf9dcddaef2
yuji2005yy <yuji2005yy@gmail.com>
2026-01-24 19:09:14 +0900

    [Add] 画面切り替えの機能を追加

---
+++ Assets/script/1217/BackGroundUI.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+/// <summary>
+///  背景画像のスクロール
+/// </summary>
+public class BackGroundUI : MonoBehaviour
+{//２枚の画像を用意して、端まで行ったらもう片方の端にワープしてそれを繰り返す
+    
+    [SerializeField] RectTransform[] _backGroundUI = new RectTransform[2];
+
+    // Update is called once per frame
+    void Update()
+    {
+        //_backGroundUI[0].position -= Vector3.right;
+
+        for (int i = 0; i < _backGroundUI.Length; i++)
+        {
+            _backGroundUI[i].anchoredPosition -= Vector2.right * Time.deltaTime;
+
+            if (_backGroundUI[i].position.x <= -19)
+            {
+                Vector2 pos = _backGroundUI[i].anchoredPosition;
+                pos.x = 19;
+                _backGroundUI[i].anchoredPosition = pos;
+            }
+        }
+    }
+}
