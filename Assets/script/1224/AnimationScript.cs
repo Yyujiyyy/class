7831b85547857af43c6a99b824830cc7dd05cd59
yuji2005yy <yuji2005yy@gmail.com>
2026-01-26 20:21:50 +0900

    [Add] 連番アニメーションを追加

---
+++ Assets/script/1224/AnimationScript.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// 連番アニメーション
+/// </summary>
+public class AnimationScript : MonoBehaviour
+{
+    [SerializeField] Sprite[] frames;
+    private float frameRates = 0.35f;
+
+    SpriteRenderer sr;
+    int index;
+    float timer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sr = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // 連番アニメーション
+        timer += Time.deltaTime;
+
+        if (timer >= frameRates)
+        {
+            timer = 0f;
+            index = (index + 1) % frames.Length;
+            sr.sprite = frames[index];
+        }
+    }
+}
