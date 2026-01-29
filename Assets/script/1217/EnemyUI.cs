c9b3f35c1e6c09a56f305637f8a63a6709ee13b6
yuji2005yy <yuji2005yy@gmail.com>
2026-01-29 15:27:56 +0900

    [Add] 敵のUIを追加

---
+++ Assets/script/1217/EnemyUI.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class EnemyUI : MonoBehaviour
+{
+    [SerializeField] GameObject[] _ui = new GameObject[2];
+    private int index = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for(int i = 0; i < _ui.Length; i++)
+        {
+            _ui[i].SetActive(false);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (index < 0) index = 0;
+        if (1 < index) index = 1;
+
+
+        if(this.gameObject.activeSelf)
+        {
+            _ui[index].SetActive(true);
+
+            if (index == 0) index++;
+            if(index == 1) index--;
+        }
+    }
+}
